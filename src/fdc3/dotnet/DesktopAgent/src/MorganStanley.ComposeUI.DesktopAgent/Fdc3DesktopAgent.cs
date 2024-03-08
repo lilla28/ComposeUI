52d84d064715e245d215ca61307a149b15182e7b
lilla28 <36889371+lilla28@users.noreply.github.com>
2024-03-08 17:43:10 +0100

    chore(messaging): Added Publish-, Subscribe- and UnsubscribeResponse types on both .NET and Typescript side, fixed DesktopAgent ModuleNotFound exception when removing module from the dictionary -running the chart and grid example. (#471)
    * chore(messaging): Added Publish-, Subscribe- and UnsubscribeResponse types on both .NET and Typescript side, fixed DesktopAgent ModuleNotFound exception when removing module from the dictionary -running the chart and grid example.
    
    * chore(messaging): Removed try catch block, added exception OnStopRequest method
    
    * fix(messaging) - Fixed imports in *.ts files

--- src/fdc3/dotnet/DesktopAgent/src/MorganStanley.ComposeUI.DesktopAgent/Fdc3DesktopAgent.cs
+++ src/fdc3/dotnet/DesktopAgent/src/MorganStanley.ComposeUI.DesktopAgent/Fdc3DesktopAgent.cs
@@ -839,7 +839,15 @@ internal class Fdc3DesktopAgent : IFdc3DesktopAgentBridge
 
         if (!_runningModules.TryRemove(new(fdc3InstanceId!), out _)) //At this point the fdc3InstanceId shouldn't be null
         {
-            _logger.LogError($"Could not remove the closed window with instanceId: {fdc3InstanceId}.");
+            var fdc3InstanceId = GetFdc3InstanceId(instance);
+            if (!_runningModules.TryRemove(new(fdc3InstanceId), out _))
+            {
+                _logger.LogError($"Could not remove the closed window with instanceId: {fdc3InstanceId}.");
+            }
+        }
+        catch (Fdc3DesktopAgentException exception)
+        {
+            _logger.LogError(exception, $"Exception thrown while removing module: {instance.Manifest.Id}, {instance.Manifest.Name} from running instances in FDC3DesktopAgent.");
         }
 
         if (_pendingStartRequests.TryRemove(instance.StartRequest, out var taskCompletionSource))
--- src/messaging/dotnet/test/Client.Tests/Client/MessageRouterClient.Tests.cs
+++ src/messaging/dotnet/test/Client.Tests/Client/MessageRouterClient.Tests.cs
@@ -10,6 +10,7 @@
 // or implied. See the License for the specific language governing permissions
 // and limitations under the License.
 
+using System.Diagnostics;
 using System.Linq.Expressions;
 using Microsoft.Extensions.Logging;
 using MorganStanley.ComposeUI.Messaging.Abstractions;
