56e25fb6962e5283e0313844d28617dafffaa558
lilla28 <orbanlilla28@gmail.com>
2024-07-08 16:15:00 +0200

    fix(nx-monorepo): Updating names, packages, documentations; rebasing branch

--- prototypes/multi-module-prototype/examples/multi-module-example/ModulesPrototype/scripts/npm-builds.ps1
+++ prototypes/multi-module-prototype/examples/multi-module-example/ModulesPrototype/scripts/npm-builds.ps1
@@ -1,3 +1,3 @@
 # Building the MessageRouter-web-client, because without it the chart couldn't be started.
 cd $PSScriptRoot\..\..\..\..\..\..
-npx lerna run build --scope '{@morgan-stanley/composeui-messaging-client,@morgan-stanley/composeui-example-chart}'
+npx nx run-many --target=build --projects=@morgan-stanley/composeui-messaging-client,@morgan-stanley/composeui-example-chart --maxParallel=100
